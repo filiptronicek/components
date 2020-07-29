a2f0a775271e7041def0d51cfcccf57deefb5bc4
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-29 14:27:05 +0000

    Initial stuff

---
+++ branding.js
@@ -0,0 +1,19 @@
+class Brand extends HTMLElement {
+    connectedCallback() {
+
+        if (!this.hasAttribute('profiles')) {
+            this.setAttribute('profiles', "filiptronicek");
+        }
+        const ppl = this.getAttribute('profiles').split(",")
+        let pplStr;
+        pplStr = ppl.join(", ").replace(",", " &")
+        this.innerHTML = `
+        <footer>
+            <hr />
+            Made with ❤️ and code by ${pplStr}
+        </footer>
+    `;
+    }
+}
+
+customElements.define("site-branding", Brand);
